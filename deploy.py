f1d162c423032e095c4db5ac9212beb9138802c6
Jai-Keshav-Sharma <148455549+Jai-Keshav-Sharma@users.noreply.github.com>
2024-10-18 08:27:16 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -9,93 +9,83 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-
-# loading the saved models
-
+# Loading the saved models
 heart_disease_model = pickle.load(open('heart_disease_prediction_logisticRegression.sav', 'rb'))
 
-
-
-
-# sidebar for navigation
+# Sidebar for navigation
 with st.sidebar:
-    
     selected = option_menu('Heart Disease Prediction System',
-                          
-                          [
-                           'Heart Disease Prediction',
-                           ],
-                          icons=['activity','heart','person'],
-                          default_index=0)
-    
-    
-
-
+                           ['Heart Disease Prediction'],
+                           icons=['activity', 'heart', 'person'],
+                           default_index=0)
 
 # Heart Disease Prediction Page
-if (selected == 'Heart Disease Prediction'):
+if selected == 'Heart Disease Prediction':
     
-    # page title
+    # Page title
     st.title('Heart Disease Prediction using ML')
     
+    # Input fields arranged in columns
     col1, col2, col3 = st.columns(3)
     
     with col1:
-        age = st.text_input('Age')
+        age = st.number_input('Age', min_value=1, max_value=120, step=1)
         
     with col2:
-        sex = st.text_input('Sex')
+        sex = st.selectbox('Sex (1 = Male, 0 = Female)', options=[1, 0])
         
     with col3:
-        cp = st.text_input('Chest Pain types')
+        cp = st.selectbox('Chest Pain Types (0-3)', options=[0, 1, 2, 3])
         
     with col1:
-        trestbps = st.text_input('Resting Blood Pressure')
+        trestbps = st.number_input('Resting Blood Pressure (in mm Hg)', step=1)
         
     with col2:
-        chol = st.text_input('Serum Cholestoral in mg/dl')
+        chol = st.number_input('Serum Cholesterol in mg/dl', step=1)
         
     with col3:
-        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+        fbs = st.selectbox('Fasting Blood Sugar > 120 mg/dl (1 = True, 0 = False)', options=[1, 0])
         
     with col1:
-        restecg = st.text_input('Resting Electrocardiographic results')
+        restecg = st.selectbox('Resting Electrocardiographic Results (0-2)', options=[0, 1, 2])
         
     with col2:
-        thalach = st.text_input('Maximum Heart Rate achieved')
+        thalach = st.number_input('Maximum Heart Rate Achieved', step=1)
         
     with col3:
-        exang = st.text_input('Exercise Induced Angina')
+        exang = st.selectbox('Exercise Induced Angina (1 = Yes, 0 = No)', options=[1, 0])
         
     with col1:
-        oldpeak = st.text_input('ST depression induced by exercise')
+        oldpeak = st.number_input('ST Depression Induced by Exercise', format="%.2f")
         
     with col2:
-        slope = st.text_input('Slope of the peak exercise ST segment')
+        slope = st.selectbox('Slope of the Peak Exercise ST Segment (0-2)', options=[0, 1, 2])
         
     with col3:
-        ca = st.text_input('Major vessels colored by flourosopy')
+        ca = st.selectbox('Major Vessels Colored by Fluoroscopy (0-3)', options=[0, 1, 2, 3])
         
     with col1:
-        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+        thal = st.selectbox('Thal (1 = Fixed Defect, 2 = Normal, 3 = Reversible Defect)', options=[1, 2, 3])
         
-        
-     
-     
-    # code for Prediction
+    # Code for Prediction
     heart_diagnosis = ''
     
-    # creating a button for Prediction
-    
+    # Creating a button for Prediction
     if st.button('Heart Disease Test Result'):
-        heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg,thalach,exang,oldpeak,slope,ca,thal]])                          
-        
-        if (heart_prediction[0] == 1):
-          heart_diagnosis = 'The person is having heart disease'
-        else:
-          heart_diagnosis = 'The person does not have any heart disease'
+        try:
+            # Convert inputs to a list of numeric values
+            input_data = [[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]]
+            
+            # Make prediction
+            heart_prediction = heart_disease_model.predict(input_data)
+            
+            # Display result
+            if heart_prediction[0] == 1:
+                heart_diagnosis = 'The person is having heart disease'
+            else:
+                heart_diagnosis = 'The person does not have heart disease'
+                
+        except ValueError:
+            st.error("Please enter valid numeric values.")
         
     st.success(heart_diagnosis)
-        
-    
-    
